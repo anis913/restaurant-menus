13e6607cd193f3c6c5d248dde1e413d0f5bff798
anis913 <anisislam913@gmail.com>
2023-03-02 23:10:34 +0000

    Finale!

--- models/index.js
+++ models/index.js
@@ -1,4 +1,12 @@
 const {Restaurant} = require('./Restaurant')
 const {Menu} = require('./Menu')
+const {Item} = require('./Item')
 
-module.exports = { Restaurant, Menu }
+
+Menu.belongsTo(Restaurant);
+Restaurant.hasMany(Menu);
+
+Item.belongsToMany(Menu, {through: "item_menu"});
+Menu.belongsToMany(Item, {through: "item_menu"});
+
+module.exports = { Restaurant, Menu, Item }
